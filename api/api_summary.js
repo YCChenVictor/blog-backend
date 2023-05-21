fdaff5c780e10d522da81aca87808b96595dbe1f
Victor Chen <victor.chen@openapply.com>
2023-05-21 15:00:09 +0800

    init basic youtube api

---
+++ api/api_summary.js
@@ -0,0 +1,31 @@
+import { google } from "googleapis";
+const youtube = google.youtube({ // going to extract to separate file
+  version: "v3",
+  auth: apiKey,
+});
+
+const apis = function(app) {
+  // hello world
+  app.get('/', (req, res) => {
+    console.log(req.query)
+    res.send('Hello World!')
+  })
+
+  // youtube
+  app.get("/search-youtube-with-googleapis", async (req, res, next) => {
+    try {
+      const searchQuery = req.query.search_query;
+      const response = await youtube.search.list({
+        part: "snippet",
+        q: searchQuery,
+      });
+  
+      const titles = response.data.items.map((item) => item.snippet.title);
+      res.send(titles);
+    } catch (err) {
+      next(err);
+    }
+  });
+}
+
+export default apis
