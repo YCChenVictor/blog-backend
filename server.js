bad1fbe2021379dc77401053a76335dd90db025f
Victor Chen <victor.chen@openapply.com>
2023-05-21 14:59:45 +0800

    init server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+// init
+import express from 'express';
+const app = express()
+
+// // parse body
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.json())
+
+// // passport
+// const passport = require('./middleware/passport.js');
+// app.use(passport.initialize());
+
+// // CORS
+// const cors = require('./middleware/cors.js');
+// app.use(cors);
+
+// API
+import api from './api/api_summary.js';
+if (process.env.NODE_ENV === 'development') {
+  app.listen(5000, () => {
+    api(app)
+    console.log('now listening to 5000')
+  })
+} else if (process.env.NODE_ENV === 'test') {
+  app.listen(8080, () => {
+    api(app)
+  })
+} else {
+  // TODO for production
+}
+
+module.exports = app
