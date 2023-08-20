c7dd549ff54a1b289593c07c3f13bbb63387cb62
Victor Chen <victor.chen@openapply.com>
2023-08-20 17:00:28 +0800

    add GPT

--- apis/gpt.js
+++ apis/gpt.js
@@ -1,28 +1,24 @@
 const passport = require('../middleware/passport.js')
-// const API_KEY = 'YOUR_OPENAI_API_KEY';
-// const API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+const { OpenAI } = require("openai")
 
 const gptApi = (app) => {
   app.get('/gpt-init', passport.authenticate('jwt'), (req, res) => {
     res.status(200).json({ loggedIn: true })
   })
 
-  app.post('/create-article', passport.authenticate('jwt'), async (prompt) => {
-    try {
-      const response = await axios.post(API_URL, {
-        prompt: prompt,
-        max_tokens: 100, // Adjust the length of the generated text
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${API_KEY}`,
-        },
-      });
-  
-      return response.data.choices[0].text;
+  app.post('/create-article', passport.authenticate('jwt'), async (req) => {
+    try { // write the test for this API
+      const openai = new OpenAI({
+        apiKey: process.env.GPT_KEY,
+      })
+      const chat_completion = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo", // we can change to better model
+          messages: [{ role: "user", content: req.body.prompt }],
+      })
+      console.log(chat_completion.choices)
     } catch (error) {
-      console.error('Error generating text:', error);
-      return '';
+      console.error('Error generating text:', error)
+      return ''
     }
   })
 }
