5d1ad43a60192a2edd82e1745c31feb7864e4d6a
Victor Chen <victor.chen@openapply.com>
2023-08-27 15:27:27 +0800

    practice dsa

---
+++ tmp/dsa/array/spec/one-away.spec.js
@@ -0,0 +1,16 @@
+const oneAway = require('../examples/one-away.js')
+
+describe('One Away', () => {
+  test('paie, pie', () => {
+    expect(oneAway('paie', 'pie')).toEqual(true)
+  })
+  test('paies, paie', () => {
+    expect(oneAway('paies', 'paie')).toEqual(true)
+  })
+  test('paie, baie', () => {
+    expect(oneAway('paie', 'baie')).toEqual(true)
+  })
+  test('paie, bake', () => {
+    expect(oneAway('paie', 'bake')).toEqual(false)
+  })
+})
