89fc236d37d1419afb5ff073c152aff09495759c
Victor Chen <victor.chen@openapply.com>
2023-08-30 18:57:50 +0800

    string compression dsa

---
+++ tmp/dsa/array/spec/string-compression.spec.js
@@ -0,0 +1,10 @@
+const stringCompression = require('../examples/string-compression.js')
+
+describe('stringCompression', () => {
+  test('aabcccccaaa', () => {
+    expect(stringCompression('aabcccccaaa')).toEqual('a2b1c5a3')
+  })
+  test('a', () => {
+    expect(stringCompression('a')).toEqual('a')
+  })
+})
