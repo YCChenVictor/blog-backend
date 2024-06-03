d3cee759b75a5f2376f00a4c6b45d2c87697a5c6
YCChen <victor.yong.jhe.chen@gmail.com>
2024-06-03 17:11:05 +0800

    use start point instead

--- src/controllers/nodeGraphController.ts
+++ src/controllers/nodeGraphController.ts
@@ -4,10 +4,10 @@ import path from 'path';
 import fs from 'fs';
 import randomColor from 'randomcolor';
 import { Request, Response, NextFunction } from 'express';
-import { processPage, crawl } from 'crawl-website-connectedness';
+import main from 'crawl-website-connectedness';
 
 export const create = async (req: Request, res: Response, next: NextFunction) => {
-  const queue = [`http://localhost:3000/blog/software/main`];
-  const visited = new Set('');
-  crawl(queue, visited);
+  const startPoint = 'http://localhost:3000/blog/software/main';
+  const result = main(startPoint)
+  console.log(result);
 };
